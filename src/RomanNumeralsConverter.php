3cc17970c674795e3a9b8cec58233fb28f34a240
Thiago van Dieten <nktakumi@gmail.com>
2014-11-08 00:57:30 +0100

    Now it can count to 5000

--- src/RomanNumeralsConverter.php
+++ src/RomanNumeralsConverter.php
@@ -2,11 +2,18 @@
 
 class RomanNumeralsConverter
 {
+    /**
+     * @var array
+     */
     protected static $lookup = [
         1000 => 'M',
+        900 => 'CM',
         500 => 'D',
+        400 => 'CD',
         100 => 'C',
+        90 => 'XC',
         50 => 'L',
+        40 => 'XL',
         10 => 'X',
         9 => 'IX',
         5 => 'V',
@@ -14,6 +21,10 @@ class RomanNumeralsConverter
         1 => 'I'
     ];
 
+    /**
+     * @param $number
+     * @return string
+     */
     public function convert($number)
     {
         $solution = '';
